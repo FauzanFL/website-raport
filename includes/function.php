edb44ad378c40c0063a3253a860d40bc0acb52ca
Daffarr <77198942+Daffarr@users.noreply.github.com>
2022-12-27 23:01:47 +0700

    Create function.php

---
+++ includes/function.php
@@ -0,0 +1,29 @@
+<?php
+
+//============= DATABASE HELPER =============//
+
+function query($query){
+global $koneksi;
+return mysqli_query($koneksi, $query);
+}
+
+function confirmQuery($result){
+global $koneksi;
+if(!$result){
+die('QUERY FAILED ' . mysqli_error($koneksi));
+}
+}
+
+function escape($value){
+global $koneksi;
+return htmlspecialchars(mysqli_real_escape_string($koneksi, $value));
+}
+
+function redirect($url){
+return header('Location: ' . $url);
+exit;
+}
+
+//============= END DATABASE HELPER =============//
+
+?>
