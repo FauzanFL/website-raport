3fe93327306e2570b291491eeacb140a3e5e9db7
Fauzan FL <fauzanarth@gmail.com>
2022-12-30 10:29:46 +0700

    membuat logic menghapus niali

---
+++ src/views/nilai/hapus-nilai.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+require('../../includes/function.php');
+
+$role = $_SESSION["role"];
+
+if (!$_SESSION["login"] || $role != $ROLE_ADMIN) {
+    redirect("../../index.php");
+    exit;
+}
+
+$id = $_GET["id"];
+
+$result = query("DELETE FROM $NILAI WHERE id=$id");
+
+if (mysqli_affected_rows($koneksi) > 0) {
+    echo "
+        <script>
+            alert('Data berhasil dihapus!');
+            document.location.href='nilai.php';
+        </script>
+    ";
+} else {
+    echo "
+        <script>
+            alert('Data gagal dihapus!');
+            document.location.href='nilai.php';
+        </script>
+    ";
+}
