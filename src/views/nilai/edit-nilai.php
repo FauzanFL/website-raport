0769e6c6d8c3f242a5ed72c8ee3980c7b6c1e229
Fauzan FL <fauzanarth@gmail.com>
2023-01-02 18:59:36 +0700

    memperbaiki input nilai

--- src/views/nilai/edit-nilai.php
+++ src/views/nilai/edit-nilai.php
@@ -89,7 +89,7 @@ $siswa = getDataById($SISWA, $nilai["id_siswa"]);
                 </div>
                 <div class="mb-3">
                     <label for="nilai" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nilai</label>
-                    <input type="text" name="nilai" id="nilai" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="<?= $nilai["nilai"]; ?>" required>
+                    <input type="number" name="nilai" id="nilai" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="<?= $nilai["nilai"]; ?>" required>
                 </div>
                 <div class="mb-3">
                     <label for="catatan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Catatan</label>
