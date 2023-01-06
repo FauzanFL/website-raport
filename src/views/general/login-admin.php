5dd42e2154bc53979ada08776f4901d0b0fd354f
Daffarr <77198942+Daffarr@users.noreply.github.com>
2023-01-06 13:33:47 +0700

    Update login-admin.php

--- src/views/general/login-admin.php
+++ src/views/general/login-admin.php
@@ -41,7 +41,7 @@ if (isset($_POST["login"])) {
 
 <body>
     <main>
-        <h1 class="text-3xl font-bold mx-2 dark:text-white">Login Admin</h1>
+        <h1 class="text-3xl font-bold mx-2 dark:text-dark">Login Admin</h1>
         <form method="POST">
             <?php if ($wrong) : ?>
                 <div class="flex p-2 mb-2 text-sm text-red-700 bg-red-100 rounded-md dark:bg-red-200 dark:text-red-800" role="alert">
